3d80d46481dd36a0626a57ffe567ea5c2e1ebc24
Florian Thienel <florian.thienel@inovex.de>
2024-09-30 15:06:43 +0200

    Bump Gardener to v1.97.4

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -14,7 +14,7 @@ PROJECT_ROOT=$(dirname $0)/..
 
 git config --global --add safe.directory /go/src/github.com/metal-stack/gardener-extension-audit
 
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
+bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/kube_codegen.sh \
   deepcopy,defaulter \
   github.com/metal-stack/gardener-extension-audit/pkg/client \
   github.com/metal-stack/gardener-extension-audit/pkg/apis \
@@ -22,7 +22,7 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   "audit:v1alpha1" \
   --go-header-file "${PROJECT_ROOT}/hack/boilerplate.txt"
 
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
+bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/kube_codegen.sh \
   conversion \
   github.com/metal-stack/gardener-extension-audit/pkg/client \
   github.com/metal-stack/gardener-extension-audit/pkg/apis \
@@ -31,7 +31,7 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   --extra-peer-dirs=github.com/metal-stack/gardener-extension-audit/pkg/apis/audit,github.com/metal-stack/gardener-extension-audit/pkg/apis/audit/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/conversion,k8s.io/apimachinery/pkg/runtime \
   --go-header-file "${PROJECT_ROOT}/hack/boilerplate.txt"
 
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
+bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/kube_codegen.sh \
   deepcopy,defaulter \
   github.com/metal-stack/gardener-extension-audit/pkg/client/componentconfig \
   github.com/metal-stack/gardener-extension-audit/pkg/apis \
@@ -39,7 +39,7 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   "config:v1alpha1" \
   --go-header-file "${PROJECT_ROOT}/hack/boilerplate.txt"
 
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
+bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/kube_codegen.sh \
   conversion \
   github.com/metal-stack/gardener-extension-audit/pkg/client/componentconfig \
   github.com/metal-stack/gardener-extension-audit/pkg/apis \
